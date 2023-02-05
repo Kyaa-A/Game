35b05be995f846c7b5997d1d694a28acab2553f5
Kyaa-A <asnaripacalna@gmail.com>
2023-02-05 13:06:45 +0700

    init

--- Guessing_Game.java
+++ Guessing_Game.java
@@ -7,27 +7,35 @@ public class Guessing_Game {
         Scanner scan = new Scanner(System.in);
         Random rand = new Random();
         boolean win = false;
+        int numTries = 0;
+        String limiter = " ";
 
         System.out.println("[x] Computer has chosen a number");
         int numberToGuess = rand.nextInt(9) + 1;
 
         while (win == false) {
 
-            System.out.print("[?] Guess a number from 1-9: ");
+            System.out.print("\n[?] Guess a number from 1-9: ");
             int guess = scan.nextInt();
+
+            numTries++;
+
             if (guess == numberToGuess) {
-                System.out.println("You did it!");
+                System.out.println("\nYou did it!");
                 win = true;
             } else if (guess > numberToGuess) {
                 System.out.println("Your guess is too high!");
-
+                limiter = "lost";
             } else if (guess < numberToGuess) {
                 System.out.println("Your guess is too low!");
-
+                limiter = "lost";
             }
 
+            if(numTries == 3 && limiter.equals("lost") ){
+                System.out.println("\nYou lost!");
+                break;
+            }
         }
 
     }
-
 }
